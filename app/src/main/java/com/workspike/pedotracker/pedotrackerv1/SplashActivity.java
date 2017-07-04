a3adfbd04c6cf00d2ec0de2cd623c5536a313020
chamlipriyashan <chamlipriyashan@gmail.com>
2017-07-04 22:34:53 +0530

    home ws

--- app/src/main/java/com/workspike/pedotracker/pedotrackerv1/SplashActivity.java
+++ app/src/main/java/com/workspike/pedotracker/pedotrackerv1/SplashActivity.java
@@ -3,11 +3,30 @@ package com.workspike.pedotracker.pedotrackerv1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Intent intent = new Intent(this,  MainActivity.class);
+        startActivity(intent);
+        finish();
     }
+
 }
