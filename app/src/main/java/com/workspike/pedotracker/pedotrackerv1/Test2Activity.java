54765d5f6be72899b574a050ec7308ec58ae271c
chamlipriyashan <chamlipriyashan@gmail.com>
2017-11-01 13:05:36 +0530

    home ws

--- app/src/main/java/com/workspike/pedotracker/pedotrackerv1/Test2Activity.java
+++ app/src/main/java/com/workspike/pedotracker/pedotrackerv1/Test2Activity.java
@@ -12,8 +12,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.CornerPathEffect;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Point;
+import android.graphics.PointF;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.RectF;
@@ -27,9 +30,11 @@ import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.workspike.pedotracker.pedotrackerv1.my_clases.MainActivity;
 import com.workspike.pedotracker.pedotrackerv1.not_used.DeviceControlActivity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
@@ -113,7 +118,7 @@ public class Test2Activity extends AppCompatActivity {
 
 
 
-
+   Test2Activity.HeartGraphView v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -124,7 +129,7 @@ public class Test2Activity extends AppCompatActivity {
 
         RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.canvas);
         RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.FILL_PARENT);
-        HeartGraphView v = new HeartGraphView(this);
+         v = new Test2Activity.HeartGraphView(this);
         v.setLayoutParams(lp);
         relativeLayout.addView(v);
 
@@ -216,7 +221,9 @@ public class Test2Activity extends AppCompatActivity {
         });
     }
 
-
+    int globle_i =1;
+    int[] mydataX = new int[1001];
+    int[] mydataY = new int[1001];
     String previousString1="";
     String previousString2="";
 
@@ -225,12 +232,44 @@ public class Test2Activity extends AppCompatActivity {
         if (data != null) {
 
 
-            tv_console.setText(previousString2+"\n"+previousString1+"\n"+data);
+
+            List<String> items = Arrays.asList(data.split(","));
+            //  String[] animalsArray = data.split("\\s*,\\s*");
+            //  String   aa= (items.get(1));
+            //  String bb= (items.get(2));
+            //  System.out.println(animalsArray[0] +"  ---  "+animalsArray[1]+"     " + animalsArray[2]  );
 
 
+            int length_ofpath= Integer.parseInt(items.get(0));
+            int angle360= Integer.parseInt(items.get(1));
+
+            System.out.println(length_ofpath);
+            System.out.println(angle360);
+
+
+            mydataX[ globle_i] = (int) (2*length_ofpath*Math.cos(angle360));
+            mydataY[ globle_i] = (int) (2*length_ofpath*Math.sin(angle360));
+
+            v.invalidate();
+
+
+            tv_console.setText(previousString2+""+previousString1+""+data);
+//  console_tv.setText(previousString8+"\n"+previousString7+"\n"+previousString6+"\n"+previousString5+"\n"+previousString4+"\n"+previousString3+"\n"+previousString2+"\n"+previousString1+"\n"+data);
+//            previousString8=previousString7;
+//            previousString7=previousString6;
+//            previousString6=previousString5;
+//
+//            previousString5=previousString4;
+//            previousString4=previousString3;
+//            previousString3=previousString2;
             previousString2=previousString1;
             previousString1=data;
+
+        }else{
+            System.out.println("connect device");
         }
+
+        globle_i++;
     }
 
 
@@ -239,87 +278,161 @@ public class Test2Activity extends AppCompatActivity {
 
     public class HeartGraphView extends View {
 
-        private List<Point> mPoints = new ArrayList<Point>();
-        private Paint mPaint;
-        int min = 100;
-        int max = 480;
-        Bitmap board;
-        int randomX=0;
-        int randomY=0;
-        int[] mydataX = new int[101];
-        int[] mydataY = new int[101];
-
-
-        void init(){
-            board= BitmapFactory.decodeResource(getResources(),R.drawable.floor1map);
-            Point p1 = new Point(20, 20);
-            Point p2 = new Point(100, 100);
-            mPoints.add(p1);
-            mPoints.add(p2);
-        }
         public HeartGraphView(Context context) {
             super(context);
             // TODO Auto-generated constructor stub
-            init();
+            Init_floor_map();
         }
 
 
+        Bitmap board;
+        int i=0;
+        Paint paint = new Paint();
+        int randomX=0;
+        int randomY=0;
+
+        int min = 100;
+        int max = 480;
+        public Paint mPaint;
+        public Canvas mCanvas;
+        int startX;
+        int startY;
+        int endX;
+        int endY;
+        private PointF startPoint, endPoint;
+
+        private void Init_floor_map() {
+
+            board= BitmapFactory.decodeResource(getResources(),R.drawable.floor1map);
+            paint.setColor(Color.BLUE);
+        }
+
+
+        int[] dataX = new int[30];
+        int[] dataY = new int[30];
 
         public int generatRandomPositiveNegitiveValue(int max, int min) {
             Random r = new Random();
             int ii = r.nextInt(max - min + 1) + min;
-            return (ii );
+            return (ii - 140);
         }
 
 
         public void getlocation() {
             randomX=generatRandomPositiveNegitiveValue(max, min);
             randomY=generatRandomPositiveNegitiveValue(max, min);
+
+            //  System.out.println("get location");
+            // return (ii - 140);
+
         }
+
+
+        Point start_point;
+        Rect frameToDraw;
+        RectF whereToDraw;
+        int Y0 ;
+        int X0;
+
 
         @Override
         public void onDraw(Canvas canvas) {
-            init();
-            int Y0 = canvas.getHeight();
-            int X0 = canvas.getWidth()/2;
-            Point start_point= new Point(X0, Y0);
+            int w;
+            int h;
+            h = 280;
+            w = 600;
+            Y0 = canvas.getHeight();
+            X0 = canvas.getWidth()/2;
+            start_point= new Point(X0, Y0);
+            frameToDraw = new Rect(0, 0, board.getWidth(),board.getHeight());
+            whereToDraw = new RectF(0, 0, canvas.getWidth(), canvas.getHeight());
+            canvas.drawBitmap(board,frameToDraw,whereToDraw, paint);
+            int min = 0;
+            int max = 280;
 
-            mPaint = new Paint();
-            mPaint.setAntiAlias(true);
-            mPaint.setDither(true);
-            mPaint.setColor(Color.BLUE);
-            Paint paint = new Paint();
-            paint.setColor(Color.RED);
             paint.setStrokeWidth(6);
 
-            Rect frameToDraw = new Rect(0, 0, board.getWidth(),board.getHeight());
-            RectF whereToDraw = new RectF(0, 0, canvas.getWidth(), canvas.getHeight());
-            canvas.drawBitmap(board,frameToDraw,whereToDraw, paint);
-            canvas.drawLine(start_point.x,start_point.y, (canvas.getWidth()/3), 100, mPaint);//start,end
 
-            for (int i = 1; i <10; i++) {
+
+            //************************************************Graph plotin********************perfect*************************************
+     /*
+
+            dataX[0] = 0;
+            for (int i = 0; i < w / 20 - 1; i++) {
+                dataX[i + 1] = (i + 1) * w / 20;
                 getlocation();
-                mydataX[i] = randomX;
-                mydataY[i] = randomY;
-
+                dataY[w / 20 - 1] = generatRandomPositiveNegitiveValue(max, min);
+                dataY[i] = dataY[i + 1];
             }
+
+            for (int i = 0; i < w / 20 - 1; i++) {
+                // apply some transformation on data in order to map it correctly
+                // in the coordinates of the canvas
+                canvas.drawLine(dataX[i], h / 2 - dataY[i], dataX[i + 1], h / 2 - dataY[i + 1], paint);
+                canvas.drawColor(Color.WHITE, PorterDuff.Mode.MULTIPLY);
+//                try {
+//                    Thread.sleep(10);
+//                } catch (InterruptedException e) {
+//                    // TODO Auto-generated catch block
+//                    e.printStackTrace();
+//                }
+                invalidate();
+            }
+*/
+//************************************************Graph plotin********************perfect*************************************
+
+
+
+            canvas.drawLine(start_point.x,start_point.y, (canvas.getWidth()/3), 100, paint);//start,end
 
             mydataX[0] = start_point.x;
             mydataY[0] = start_point.y;
 
+            canvas.drawLine(mydataX[0],mydataY[0],mydataX[1], mydataY[1], paint);
+//            for (int i = 0; i < X0 ; i++) {
+//                dataX[i + 1] = (i + 1) * X0 / 20;
+//                //dataX[i]=getlocation(10,100);
+//                dataY[X0 / 20 - 1] = generatRandomPositiveNegitiveValue(max, min);
+//                dataY[i] = dataY[i + 1];
+//            }
 
-            for (int i = 0; i < 10; i++) {
-                canvas.drawLine(mydataX[i], mydataY[i],mydataX[i+1], mydataY[i+1], paint);
+            for (int i = 1; i < globle_i-1; i++) {
+                canvas.drawLine(X0+mydataX[i], Y0+mydataY[i],X0+mydataX[i+1], Y0+mydataY[i+1], paint);
                 canvas.drawColor(Color.WHITE, PorterDuff.Mode.MULTIPLY);
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-                invalidate();
+                //try {
+                //       Thread.sleep(10);
+                //  } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                //      e.printStackTrace();
+                //   }
+                // invalidate();
             }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         }
     }
+
+
+
+
+
+
+
+
 }
